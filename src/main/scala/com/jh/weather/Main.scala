626af8e9f57e760eef8dce7ff3ce61a33d51800b
Kevin Won <kevin@gridcure.com>
2025-09-24 20:30:15 -0700

    adding tests

--- src/main/scala/com/jh/weather/Main.scala
+++ src/main/scala/com/jh/weather/Main.scala
@@ -1,9 +1,9 @@
 package com.jh.weather
 
 /**
- * For this assignment, I'm using a single file to express all of the various bits. This is generally not
- * best-practice (almost every type should have its own file), but I think for this particular task, it is
- * easier to read and follow if it's all just in-lime.
+ * For this assignment, I'm using a single file to express all of the various bits (except for tests). This is
+ * generally not best-practice (almost every type should have its own file), but I think for this particular task, it
+ * is easier to read and follow if it's all just in-lime.
  *
  * I'm using the tagless final approach here (i.e. F[_]), which is total overkill and needlessly complicated for this
  * little service, but it does allow me to follow what I think is a best practice when working with the TypeLevel stack.
@@ -12,26 +12,27 @@ package com.jh.weather
  */
 
 import cats.data.{EitherT, Kleisli}
-import cats.effect.{Async, ExitCode, IO, IOApp, Resource}
+import cats.effect.*
 import cats.syntax.all.*
 import com.comcast.ip4s.*
 import com.jh.weather
-import io.circe._
-import io.circe.generic.semiauto._
-import org.http4s.circe.jsonEncoderOf
-import org.http4s.{EntityEncoder, HttpRoutes}
+import io.circe.*
+//import io.circe.derivation.{}
+import io.circe.generic.semiauto.*
+import io.circe.parser.decode
+import org.http4s.circe.{accumulatingJsonOf, jsonEncoderOf}
 import org.http4s.dsl.Http4sDsl
-import org.http4s.implicits.*
+import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.ember.server.*
+import org.http4s.implicits.*
 import org.http4s.server.Server
-
-import scala.Console
+import org.http4s.{EntityDecoder, EntityEncoder, HttpRoutes}
 
 // both Latitude and Longitude are expressed as Case Classes which are just wrappers over floats. This
 // pseudo strong-typing decreases the chance of transposing at Latitude for a Longitude and visa-versa.
+// note, these classes, if this was real, should have bounds checks so that only valid lat/lon values are accepted.
 case class Latitude(value: Float)
 case class Longitude(value: Float)
-
 
 /**
  * The domain of all possible runtime environments
@@ -45,13 +46,15 @@ enum RuntimeEnvironment:
 trait Logger[F[_]]:
   def info(msg: String): F[Unit]
   def error(msg: String): F[Unit]
+  def verbose(msg: String): F[Unit]
 
 object Logger:
   /** A console printing stand-in for a real logger
    */
-  def apply[F[_]: Async] = new Logger[F] {
+  def apply[F[_] : Async] = new Logger[F] {
     def info(msg: String): F[Unit] = Async[F].delay(Console.println(s"INFO: ${msg}"))
     def error(msg: String): F[Unit] = Async[F].delay(Console.println(s"ERROR: ${msg}"))
+    def verbose(msg: String): F[Unit] = Async[F].delay(Console.println(s"VERBOSE: ${msg}"))
   }
 
 /**
@@ -65,13 +68,15 @@ trait WeatherServiceConfig:
 object WeatherServiceConfig:
 
   /** Figures out the correct configuration for the given runtimeEnvironment
-  */
+   */
   def fetchForRuntimeEnvironment(re: RuntimeEnvironment): Either[Throwable, WeatherServiceConfig] =
     re match
       case RuntimeEnvironment.Localhost =>
         new WeatherServiceConfig {
           val runtimeEnvironment: RuntimeEnvironment = re
-          val nationalWeatherServiceUri: String = "todo"
+
+          // example: https://api.weather.gov/points/39.7456,-97.0892
+          val nationalWeatherServiceUri: String = "https://api.weather.gov/points/"
         }.asRight
       case x =>
         Throwable(s"The environment '$x' does not have a config defined").asLeft
@@ -80,7 +85,49 @@ object WeatherServiceConfig:
  * An abstraction against the exterior HTTP service we will be calling to get weather data.
  */
 trait NationalWeatherService[F[_]]:
-  def fetchForecast(latitude: Latitude, longitude: Longitude): NationalWeatherService.Forecast
+  def fetchForecast(latitude: Latitude, longitude: Longitude): F[Either[Throwable, Forecast]]
+
+object NationalWeatherService:
+
+  /** return sub-property of call to NAS /points */
+  case class NwsPointProperties(
+                                 // this is the URI to pull the forecast, not the forecast itself
+                                 forecast: String
+                               )
+
+  /** return type of call to NAS /points */
+  case class NwsPoint(
+                       properties: NwsPointProperties
+                     )
+
+  given Decoder[NwsPoint] = deriveDecoder[NwsPoint]
+
+  object NwsPoint:
+    def fromJson(str: String): Either[Throwable, NwsPoint] = decode[NwsPoint](str).leftMap(
+      e => Throwable(s"could not parse JSON: ${e.getMessage}")
+    )
+
+  case class NwsForecastPropertyPeriod(
+                                        number: Int,
+                                        temperature: Float,
+                                        shortForecast: String
+                                      )
+
+  case class NwsForecastProperties(
+                                    periods: List[NwsForecastPropertyPeriod]
+                                  )
+
+  case class NwsForecast(
+                          properties: NwsForecastProperties
+                        ):
+
+    val periodOne: Option[NwsForecastPropertyPeriod] =
+      this.properties.periods.find(_.number == 1)
+
+  object NwsForecast:
+    def fromJson(str: String): Either[Throwable, NwsForecast] = decode[NwsForecast](str).leftMap(t =>
+      Throwable(s"error deserializing NWS Forecast JSON: ${t.getMessage}")
+    )
 
   /**
    * Names of metrics for this service
@@ -88,26 +135,104 @@ trait NationalWeatherService[F[_]]:
   val metric_fetch_success = "nws_fetch_success"
   val metric_fetch_failure = "nws_fetch_failure"
 
-object NationalWeatherService:
+  given Decoder[NwsPointProperties] = deriveDecoder[NwsPointProperties]
+  given Decoder[NwsForecastPropertyPeriod] = deriveDecoder[NwsForecastPropertyPeriod]
+  given Decoder[NwsForecastProperties] = deriveDecoder[NwsForecastProperties]
+  given Decoder[NwsForecast] = deriveDecoder[NwsForecast]
 
-  enum ForecastCharacterization:
-    case ReallyCold, Cold, Brisk, Mild, Warm, Hot, Inferno
-    def fromTemperature(f: Float): ForecastCharacterization = ???
+  def apply[F[_] : Async](
+                           config: WeatherServiceConfig
+                         )(using metrics: Metrics[F], logger: Logger[F]): F[NationalWeatherService[F]] =
 
+    // set up the automagic JSON -> Type converters
+    given EntityDecoder[F, NwsPoint] = accumulatingJsonOf[F, NwsPoint]
+    given EntityDecoder[F, NwsForecast] = accumulatingJsonOf[F, NwsForecast]
+
+    EmberClientBuilder
+      .default[F]
+      .build
+      .use { httpClient =>
+        Async[F].pure {
+          new NationalWeatherService[F]:
+            override def fetchForecast(latitude: Latitude, longitude: Longitude): F[Either[Throwable, Forecast]] =
+              val pointUri = config.nationalWeatherServiceUri + s"${latitude.value},${longitude.value}"
+
+              (for
+                _ <- logger.verbose(s"pulling NWS weather at point '$pointUri'").attemptT
+                // this call returns back the URI we use to pull the actual forecast
+                point <- httpClient.expect[NwsPoint](pointUri).attemptT
+                // use the derived forecast URI to pull the forecast
+                nwsForecast <- httpClient.expect[NwsForecast](point.properties.forecast).attemptT
+                _ <- logger.verbose(s"successfully pulled forecast for '$pointUri'").attemptT
+                _ <- metrics.uptick(metric_fetch_success).attemptT
+                jhForecast <- EitherT.fromEither[F](Forecast.fromNws(nwsForecast))
+              yield jhForecast)
+                .onError { error =>
+                  for
+                    _ <- logger.error(s"failed pulling forecast: ${error.getMessage}").attemptT
+                    _ <- metrics.uptick(metric_fetch_failure).attemptT
+                    e <- EitherT.leftT[F, Forecast](error)
+                  yield e
+                }.value
+        }
+      }
+
+/** return type from our service call */
+case class Forecast(characterization: Forecast.ForecastCharacterization, shortForecast: Forecast.ShortForecast)
+
+object Forecast:
+
+  import NationalWeatherService.*
   type ShortForecast = String
-  case class Forecast(characterization: ForecastCharacterization, shortForecast: ShortForecast)
+  type Fahrenheit = Float
 
-  // Forecast is used as the return type from HTTP calls; this sets up the automagic JSONification
-  given Encoder[ForecastCharacterization] = deriveEncoder[ForecastCharacterization]
+  /** this is used to boil down temperature to a 'characterization' */
+  enum ForecastCharacterization(val value: String):
+    case Frigid extends ForecastCharacterization("frigid")
+    case Cold extends ForecastCharacterization("cold")
+    case Brisk extends ForecastCharacterization("brisk")
+    case Mild extends ForecastCharacterization("mild")
+    case Warm extends ForecastCharacterization("warm")
+    case Hot extends ForecastCharacterization("hot")
+    case Inferno extends ForecastCharacterization("inferno")
+
+  object ForecastCharacterization:
+    def fromTemperatureF(f: Fahrenheit): ForecastCharacterization = f match
+      case x if x < 0.0 => Frigid
+      case x if x >= 0 && x < 40 => Cold
+      case x if x >= 40 && x < 60 => Brisk
+      case x if x >= 60 && x < 70 => Mild
+      case x if x >= 70 && x < 85 => Warm
+      case x if x >= 85 && x < 110 => Hot
+      case _ => Inferno
+
+    def fromString(str: String): Either[Throwable, ForecastCharacterization] = str match
+      case Frigid.value => Frigid.asRight
+      case Cold.value => Cold.asRight
+      case Brisk.value => Brisk.asRight
+      case Mild.value => Mild.asRight
+      case Warm.value => Warm.asRight
+      case Hot.value => Hot.asRight
+      case Inferno.value => Inferno.asRight
+      case x => Throwable(s"the value '$x' is not parsable to ForecastCharacterization").asLeft
+
+  def fromNws(nws: NwsForecast): Either[Throwable, Forecast] =
+      for
+        perioedOne <- Either.fromOption(
+          nws.periodOne,
+          Throwable("no period 1 found in NWS forecast")
+        )
+        characterization <- Either.catchNonFatal(ForecastCharacterization.fromTemperatureF(perioedOne.temperature))
+      yield Forecast(characterization, perioedOne.shortForecast)
+
+  given Encoder[ForecastCharacterization] = new Encoder[ForecastCharacterization]:
+    override def apply(a: ForecastCharacterization): Json = Json.fromString(a.value)
+
   given Decoder[ForecastCharacterization] = deriveDecoder[ForecastCharacterization]
-  given Encoder[Forecast] = deriveEncoder[Forecast]
-  given Decoder[Forecast] = deriveDecoder[Forecast]
 
-  def apply[F[_]: Async](
-                          config: WeatherServiceConfig
-                        )(using metrics: Metrics[F], logger: Logger[F]): NationalWeatherService[F] =
-    new NationalWeatherService[F]:
-      override def fetchForecast(latitude: Latitude, longitude: Longitude): Forecast = ???
+  given Encoder[Forecast] = deriveEncoder[Forecast]
+
+  given Decoder[Forecast] = deriveDecoder[Forecast]
 
 /**
  * An abstraction against service monitoring. This would be realized with whatever metric infra was in use,
@@ -115,7 +240,7 @@ object NationalWeatherService:
  */
 trait Metrics[F[_]]:
   /** Upticks by one the metric name given so we can follow trend lines in a tool like DataDog of whatnot
-  */
+   */
   def uptick(metricName: String): F[Unit]
 
 object Metrics:
@@ -123,7 +248,7 @@ object Metrics:
    * constructor. If real, would figure out how to spin itself up for the given runtimeEnvironment. Here, I'm just
    * giving back a fake demonstrate how it would work.
    */
-  def apply[F[_]: Async](weatherServiceConfig: WeatherServiceConfig): Either[Throwable, Metrics[F]] =
+  def apply[F[_] : Async](weatherServiceConfig: WeatherServiceConfig): Either[Throwable, Metrics[F]] =
     weatherServiceConfig.runtimeEnvironment match
       case RuntimeEnvironment.Localhost =>
         new Metrics[F] {
@@ -137,7 +262,7 @@ object Metrics:
 
 object WeatherRoutes:
 
-  /** validation of the path args
+  /** validation of path args
    */
   object FloatVar:
     def unapply(str: String): Option[Float] = str.toLowerCase match
@@ -155,20 +280,18 @@ object WeatherRoutes:
     def unapply(str: String): Option[Longitude] =
       FloatVar.unapply(str).map(Longitude.apply)
 
-  def apply[F[_] : Async](nws: NationalWeatherService[F])(using logger: Logger[F], metrics: Metrics[F]) =
+  def apply[F[_] : Async](nws: F[NationalWeatherService[F]])(using logger: Logger[F], metrics: Metrics[F]) =
     val dsl = new Http4sDsl[F] {}
     import dsl.*
 
-    given EntityEncoder[F, NationalWeatherService.Forecast] = jsonEncoderOf[NationalWeatherService.Forecast]
+    given EntityEncoder[F, Forecast] = jsonEncoderOf[Forecast]
 
     HttpRoutes.of[F] {
-      case _ @ GET -> Root / "forecast" / "short" / "lat" / LatitudeVar(latitude) / "lon" / LongitudeVar(longitude) =>
+      case _@GET -> Root / "forecast" / "short" / "lat" / LatitudeVar(latitude) / "lon" / LongitudeVar(longitude) =>
         (for
           _ <- EitherT.rightT[F, Throwable](logger.info(s"fetching lat $latitude, lon $longitude from NWS"))
-          forecast = NationalWeatherService.Forecast(
-            NationalWeatherService.ForecastCharacterization.Hot,
-            "todo"
-          )
+          weatherService <- nws.attemptT
+          forecast <- EitherT(weatherService.fetchForecast(latitude, longitude))
         yield forecast)
           .value
           .flatMap:
@@ -185,35 +308,34 @@ object WeatherRoutes:
 /**
  * The Component Harness is an abstraction across all of the components used by the system. This allows us to
  * spin up all of the components for a given environment (Dev, Test, Prod, etc) as the service spins up, housing
- * all of these environment-specific components in a single place. Thus, all instantiation is done in one go
- * when the service spins up.
+ * all of these environment-specific components in a single place.
  */
 trait ComponentHarness[F[_]]:
   val configuration: WeatherServiceConfig
   val metrics: Metrics[F]
   val logger: Logger[F]
   val routes: Kleisli[F, org.http4s.Request[F], org.http4s.Response[F]]
-
-  val nationalWeatherService: NationalWeatherService[F]
+  val nationalWeatherService: F[NationalWeatherService[F]]
 
 object ComponentHarness:
-  def fetchComponentHarnessForEnvironment[F[_]: Async](
-                                                        config: WeatherServiceConfig
-                                                      ): Either[Throwable, ComponentHarness[F]] =
+  def fetchComponentHarnessForEnvironment[F[_] : Async](
+                                                         config: WeatherServiceConfig
+                                                       ): Either[Throwable, ComponentHarness[F]] =
     config.runtimeEnvironment match
       case RuntimeEnvironment.Localhost =>
         for
-          metricInstance <-  Metrics.apply[F](config)
+          metricInstance <- Metrics.apply[F](config)
         yield new ComponentHarness[F] {
           val configuration: WeatherServiceConfig = config
           val logger: Logger[F] = Logger.apply[F]
           val metrics = metricInstance
 
           given Logger[F] = logger
+
           given Metrics[F] = metrics
 
-          val nationalWeatherService: NationalWeatherService[F] = NationalWeatherService.apply[F](config)
-          val routes  = WeatherRoutes.apply(nationalWeatherService)
+          val nationalWeatherService: F[NationalWeatherService[F]] = NationalWeatherService.apply[F](config)
+          val routes = WeatherRoutes.apply(nationalWeatherService)
         }
       case x => Throwable(s"no ComponentHarness defined for env '$x'").asLeft
 
@@ -227,33 +349,37 @@ object Main extends IOApp:
   private def fetchRuntimeEnvironment: Either[Throwable, RuntimeEnvironment] =
     RuntimeEnvironment.Localhost.asRight
 
+  /**
+   * Entrypoint
+   */
   def run(args: List[String]): IO[ExitCode] =
     (for
-      runtimeEnvironment <- EitherT.fromEither[IO](fetchRuntimeEnvironment) 
+      runtimeEnvironment <- EitherT.fromEither[IO](fetchRuntimeEnvironment)
       config <- EitherT.fromEither[IO](WeatherServiceConfig.fetchForRuntimeEnvironment(runtimeEnvironment))
       componentHarness <- EitherT.fromEither[IO](ComponentHarness.fetchComponentHarnessForEnvironment[IO](config))
     yield componentHarness)
       .value
       .flatMap:
-          case Left(error) =>
-            // failure mode--would need to know more about target env to write something that panics loudly
-            val logger = Logger.apply[IO]
+        case Left(error) =>
+          // start up failure mode--would need to know more about target env to write something that panics loudly,
+          // or perhaps not if there was infra tooling that would handle that.
+          val logger = Logger.apply[IO]
+          for
+            _ <- logger.error(s"Failed to start: ${error.getMessage}")
+          yield ExitCode.Error
+        case Right(componentHarness) =>
+          val server: Resource[IO, Server] =
             for
-              _ <- logger.error(s"Failed to start: ${error.getMessage}")
-            yield ExitCode.Error
-          case Right(componentHarness) =>
-            val server: Resource[IO, Server] =
-              for
-                _ <- Resource.eval(componentHarness.logger.info("starting service..."))
-                server <- EmberServerBuilder
-                 .default[IO]
-                 .withHost(ipv4"0.0.0.0")
-                 .withPort(componentHarness.configuration.port)
-                 .withHttpApp(componentHarness.routes)
-                 .build
-                _ <- Resource.eval(componentHarness.logger.info("server started! Awaiting inbound requests..."))
-              yield server
+              _ <- Resource.eval(componentHarness.logger.info("starting service..."))
+              server <- EmberServerBuilder
+                .default[IO]
+                .withHost(ipv4"0.0.0.0")
+                .withPort(componentHarness.configuration.port)
+                .withHttpApp(componentHarness.routes)
+                .build
+              _ <- Resource.eval(componentHarness.logger.info("server started! Awaiting inbound requests..."))
+            yield server
 
-            server
-              .use { _ => IO.never }
-              .as(ExitCode.Success)
+          server
+            .use { _ => IO.never }
+            .as(ExitCode.Success)
